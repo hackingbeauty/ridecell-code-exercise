eb75b448fd986e69f15fdcb9e50866f195fad8c2
RichieG <richieg@1090s-MacBook-Air.local>
2017-04-11 14:00:48 -0700

    Added satori

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -3,6 +3,7 @@ import { connect }             from 'react-redux';
 import injectTapEventPlugin    from 'react-tap-event-plugin';
 import getMuiTheme             from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider        from 'material-ui/styles/MuiThemeProvider';
+import RTM                     from 'satori-sdk-js';
 
 // global styles for entire app
 import './styles/app.scss';
@@ -20,6 +21,26 @@ export class App extends Component {
 
   constructor(props) {
     super(props);
+  }
+
+  componentDidMount() {
+    const endpoint = "wss://open-data.api.satori.com";
+    const appKey = "fFd6f4fEDa8Ab6cD52c2FEBdc05F5ADE";
+    const channel = "transportation";
+
+    const rtm = new RTM(endpoint, appKey);
+    rtm.on("enter-connected", function() {
+      console.log("Connected to RTM!");
+    });
+
+    const subscription = rtm.subscribe(channel, RTM.SubscriptionMode.SIMPLE);
+    subscription.on('rtm/subscription/data', function (pdu) {
+      pdu.body.messages.forEach(function (msg) {
+        // console.log(' yoyo ' + msg);
+      });
+    });
+
+    rtm.start();
   }
 
   render() {
