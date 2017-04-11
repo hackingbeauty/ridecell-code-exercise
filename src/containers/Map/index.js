af0d0c01b5d7091d8e26f8d445845ea5e69b94c5
RichieG <richieg@1090s-MacBook-Air.local>
2017-04-11 15:14:55 -0700

    Added a map but it is not displaying

--- src/containers/Map/index.js
+++ src/containers/Map/index.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
-import { connect }          from 'react-redux';
+import React, { Component } 				from 'react';
+import { connect }          				from 'react-redux';
+import GoogleMapReact from 'google-map-react';
 
 /* component styles */
 import { styles } from './styles.scss';
@@ -9,10 +10,18 @@ class Map extends Component {
     super(props);
   }
 
+  static defaultProps = {
+    center: {lat: 59.95, lng: 30.33},
+    zoom: 11
+  };
+
   render() {
     return (
       <div className={styles}>
-        
+		<GoogleMapReact
+			defaultCenter={this.props.center}
+			defaultZoom={this.props.zoom}
+		/>
       </div>
     );
   }
